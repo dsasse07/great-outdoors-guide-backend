605303f166505fcd1fcf9584803cebb95c5e613e
Daniel Sasse <dsasse07@gmail.com>
2021-03-05 18:05:39 -0500

    initial routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "users#login"
+  post 'signin', to: 'user#signin'
+  get '/me', to: 'users#show'
+  patch "/me", to: "users#update"
 end
