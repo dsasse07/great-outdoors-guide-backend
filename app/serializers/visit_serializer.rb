a7d9658df52df83a5032011d2f0b359be25d6ca7
BoltVanderhuge <john.j.wisneski@gmail.com>
2021-03-08 20:28:49 -0500

    Response format changed to url instead of path

--- app/serializers/visit_serializer.rb
+++ app/serializers/visit_serializer.rb
@@ -8,7 +8,7 @@ class VisitSerializer < ActiveModel::Serializer
   def images
     return unless object.images.attachments
     image_urls = object.images.map do |image| 
-      rails_blob_path(image)
+      rails_blob_url(image)
     end
   
     image_urls
