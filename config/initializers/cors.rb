7bf8bb94b55b512703bb93cd870cf8f9ebb0e8e2
Daniel Sasse <dsasse07@gmail.com>
2021-03-10 18:01:03 -0500

    updating cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://great-outdoors-guide.netlify.app'
 
     resource '*',
       headers: :any,
